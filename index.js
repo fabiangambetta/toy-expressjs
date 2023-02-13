4396a16ebc4b7f786c3dea03db1fd9c713b54ac9
fabian gambetta <willian.gambetta@fing.edu.uy>
2023-02-13 20:04:36 -0300

    commit: Export application

---
+++ index.js
@@ -0,0 +1,3 @@
+const application = require('./src/application/index')
+
+module.exports = application;
--- src/application/index.js
+++ src/application/index.js
@@ -1,22 +1,21 @@
 const server = require('../server/index');
 const requestListenner = require('../requestListener/index');
 
-const application = () => {
+const application =  {
 
-    const get = (path, handler) => {
-        requestListenner().handle('GET', path, handler);
-    }
+    get: (path, handler) => {
+        if(typeof handler === 'function')
+            requestListenner.handle('GET', path, handler);
+    },
 
-    const use = (middleware) => {
-        requestListenner().use(path, hanlder);
-    }
+    use: (middleware) => {
+        requestListenner.use(path, middleware);
+    },
 
-    const listen = (port, message) => {
+    listen: (port, message) => {
         server.createServer(port, requestListenner.listenner(), message);
     }
 }
-
-
 
 
 module.exports = application;
